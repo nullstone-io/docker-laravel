5dfb56bdbdee05700277524e169a520b2774e1cd
Brad Sickles <brad.sickles@gmail.com>
2022-04-29 14:16:23 -0400

    Fix env var tests in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Install packages on boot if set (usually used for local development)
-if [ -z "${INSTALL_PACKAGES_ON_BOOT}" ]; then
+if [ -n "${INSTALL_PACKAGES_ON_BOOT}" ]; then
   echo "Installing packages..."
   composer install --no-dev
 fi
 
 # Configure DATABASE_URL if POSTGRES_URL is set
-if [ -z "${POSTGRES_URL}" ]; then
+if [ -n "${POSTGRES_URL}" ]; then
   echo "Setting DB_CONNECTION=pgsql"
   export DB_CONNECTION=pgsql
   echo "Configuring DATABASE_URL using POSTGRES_URL"
@@ -17,7 +17,7 @@ if [ -z "${POSTGRES_URL}" ]; then
 fi
 
 # Configure DATABASE_URL if MYSQL_URL is set
-if [ -z "${MYSQL_URL}" ]; then
+if [ -n "${MYSQL_URL}" ]; then
   echo "Setting DB_CONNECTION=mysql"
   export DB_CONNECTION=mysql
   echo "Configuring DATABASE_URL using MYSQL_URL"
@@ -25,7 +25,7 @@ if [ -z "${MYSQL_URL}" ]; then
 fi
 
 # Set laravel APP_ENV to NULLSTONE_ENV if set
-if [ -z "${NULLSTONE_ENV}" ]; then
+if [ -n "${NULLSTONE_ENV}" ]; then
   export APP_ENV="${NULLSTONE_ENV}"
 fi
 
